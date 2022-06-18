6d7b7f9f5e99be7f89312c9e2f6bd514a1ea313c
Lance Paje <55113864+Bilaterian@users.noreply.github.com>
2022-06-18 15:45:19 -0400

    added error checking
    checked errors for missing values in input

--- calc.py
+++ calc.py
@@ -1,3 +1,4 @@
+from ast import Break
 import pyparsing as pp
 import sys
 
@@ -30,31 +31,45 @@ def calculateResult(input):
         return input
     ##else try to parse
     elif(input[0] == '(' and input[-1] == ')'):
-        input = input[1:-1]
-        inputList = input.split(' ', 1)
+        if(len(input) <= 2):
+            input = "input too short"
+        else:
+            input = input[1:-1]
+            inputList = input.split(' ', 1)
 
-        ##split inputList[1] into two parts
-        newList = splitExpr(inputList[1])
-        inputList[1] = newList[0]
-        inputList.append(newList[1])
+            ##null checking needs to happen after split
+            if(len(inputList) != 2):
+                input = "not enough elements"
+                return input
+            
+            ##split inputList[1] into two parts
+            newList = splitExpr(inputList[1])
+            
+            ##check again for null
+            if(len(newList) != 2):
+                input = "not enough elements"
+                return input
+            
+            inputList[1] = newList[0]
+            inputList.append(newList[1])
 
-        #recursive elements
-        inputList[1] = calculateResult(inputList[1])
-        inputList[2] = calculateResult(inputList[2])
+            #recursive elements
+            inputList[1] = calculateResult(inputList[1])
+            inputList[2] = calculateResult(inputList[2])
 
-        if(inputList[1].isnumeric() and inputList[2].isnumeric()):
-            if(inputList[0] == "add"):
-                result = int(inputList[1]) + int(inputList[2])
-                input = str(result)
-            elif(inputList[0] == "multiply"):
-                result = int(inputList[1]) * int(inputList[2])
-                input = str(result)
+            if(inputList[1].isnumeric() and inputList[2].isnumeric()):
+                if(inputList[0] == "add"):
+                    result = int(inputList[1]) + int(inputList[2])
+                    input = str(result)
+                elif(inputList[0] == "multiply"):
+                    result = int(inputList[1]) * int(inputList[2])
+                    input = str(result)
+                else:
+                    input = "invalid operator"
             else:
                 input = "invalid input"
-        else:
-            input = "invalid input"
     else:
-        input = "invalid input"
+        input = "invalid input, not encased in parenthisis and not numeric"
     
     return input
 
